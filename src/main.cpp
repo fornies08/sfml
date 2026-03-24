0035b087897042cb83bd7a37bbd8a00dd8660870
Javier <apjavierboira@ieselaios.com>
2026-03-24 21:21:56 +0000

    definitivo v1.1

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,42 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Mi Proyecto SFML");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Juego");
 
-    sf::CircleShape circulo(100.f);
-    circulo.setFillColor(sf::Color(100, 200, 255));
-    circulo.setOrigin(100.f, 100.f);
-    circulo.setPosition(400.f, 300.f);
+    sf::RectangleShape player({50.f, 50.f});
+    player.setFillColor(sf::Color::Green);
+    player.setPosition(375.f, 500.f);
+
+    sf::RectangleShape enemy({40.f, 40.f});
+    enemy.setFillColor(sf::Color::Red);
+    enemy.setPosition(200.f, 0.f);
+    float enemySpeed = 200.f;
+
+    float speed = 400.f;
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float dt = clock.restart().asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event))
-        {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed &&
-                event.key.code == sf::Keyboard::Escape)
-                window.close();
-        }
 
-        circulo.rotate(1.f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            player.move(-speed * dt, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            player.move(speed * dt, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            player.move(0, -speed * dt);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            player.move(0, speed * dt);
+        enemy.move(0, enemySpeed * dt);
 
-        window.clear(sf::Color(30, 30, 30));
-        window.draw(circulo);
+        window.clear();
+        window.draw(player);
+        window.draw(enemy);
         window.display();
     }
-
-    return 0;
 }
